2ed6339a6e4fe795239b5facb64456804f57048e
dan wiseman <1590456+danwiseman@users.noreply.github.com>
2025-03-30 20:09:58 +0000

    feat(providers): add api from env get

--- services/config.go
+++ services/config.go
@@ -1,8 +1,21 @@
 // services/config.go
 package services
 
+import (
+	"fmt"
+	"os"
+)
+
 // ai configs
 func GetAPIKey(aiProvider *AIProvider) (string, error) {
 
-	return "Not implemented yet", nil
+	apiKey := os.Getenv(aiProvider.APIKeyENV)
+
+	if apiKey != "" {
+		return apiKey, nil
+	}
+
+	err := fmt.Errorf("error finding api key from: %s", aiProvider.APIKeyENV)
+
+	return "", err
 }
