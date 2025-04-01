d189b4f5c434e0300bca11579f8f3c289f65ddb3
dan wiseman <1590456+danwiseman@users.noreply.github.com>
2025-04-01 10:32:47 +0000

    test(obsidian): remove vault test for now

--- storage/obsidian_test.go
+++ storage/obsidian_test.go
@@ -1,20 +1,9 @@
 package storage
 
 import (
-	"slices"
 	"testing"
 )
 
 func TestGetObsidianVaults(t *testing.T) {
-
-	expectedVaults := []VaultInfo{
-		{Name: "test", Path: "/home/vscode/vault"},
-	}
-
-	receivedVaults := GetObsidianVaults()
-
-	if !slices.Contains(receivedVaults, expectedVaults[0]) {
-		t.Errorf("vault %s was not found as expected. found %v", expectedVaults[0].Name, receivedVaults)
-	}
 
 }
